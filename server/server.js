c0bedec90bd3cff6ebe43f348141ce0eff34ac26
Vaibhav <vaibhavlakhera1729@gmail.com>
2023-10-28 00:30:46 +0530

    server error fix

--- server/server.js
+++ server/server.js
@@ -5,24 +5,19 @@ const cors = require('cors');
 
 const URL = "mongodb+srv://vaibhav:vaibhav@cluster0.uavseiu.mongodb.net/?retryWrites=true&w=majority";
 
-
-mongoose.connect(URL).then(()=>{
+mongoose.connect(URL).then(() => {
     console.log("Connected to DB successfully");
-}).catch((e)=>{
+}).catch((e) => {
     console.log(e);
 });
 
-
-const PORT = process.env.PORT | 5000;
+const PORT = 5000; // Set the port to 5000 for local development
 const app = express();
-app.use(cors);
+
 app.use(express.json());
+app.use(cors());
 app.use(authRouter);
 
-
-
-app.listen(PORT , "0.0.0.0" , ()=> {
-    console.log(`*.* hey , listening to port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`*.* hey, listening to port ${PORT}`);
 });
-
-
