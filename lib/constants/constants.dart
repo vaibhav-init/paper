c6ec87cda27704de158def724003b410cfda8a46
Vaibhav <vaibhavlakhera1729@gmail.com>
2023-10-31 00:56:21 +0530

    quill init

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,7 +1,7 @@
 class AppIcons {
   String googleIcon = 'assets/svgs/google_logo.svg';
   String githubIcon = 'assets/svgs/github.svg';
-  String documentLogo = 'assets/svgs/document.svg';
+  String documentLogo = 'assets/svgs/document_logo.svg';
   String guestLogo = 'assets/svgs/guest.svg';
 }
 
--- lib/views/document_view.dart
+++ lib/views/document_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:paper/constants/constants.dart';
 
 class DocumentView extends ConsumerStatefulWidget {
   final String id;
@@ -10,8 +12,45 @@ class DocumentView extends ConsumerStatefulWidget {
 }
 
 class _DocumentViewState extends ConsumerState<DocumentView> {
+  TextEditingController titleController =
+      TextEditingController(text: 'Untitled Document');
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              AppIcons().documentLogo,
+              height: 40,
+              width: 30,
+            ),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: titleController,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red))),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.share),
+          ),
+        ],
+      ),
+    );
   }
 }
