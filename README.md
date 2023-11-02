c1dd5980adfd4b4d551b4123176f889f5795cd40
Vaibhav <vaibhavlakhera1729@gmail.com>
2023-11-02 21:25:59 +0530

    rf changes

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ google docs like app
 Features: 
 1. delete a document after long press  (done)
 2. reload the list after deleting . 
-3. delta -> pdf 
-4. color theme picking option 
+3. delta -> pdf
+5. Unite the snackbar
+6. change the toggle switch in the appbar 
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:paper/common/widgets/logo_button.dart';
 import 'package:paper/constants/constants.dart';
 import 'package:paper/repository/auth_repository.dart';
@@ -28,12 +29,22 @@ class LoginView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
-        child: LogoButton(
-          icon: AppIcons().googleIcon,
-          text: 'Google SignIn',
-          callback: () {
-            googleLogin(ref, context);
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/animations/intro_animation.json',
+              height: 300,
+            ),
+            const SizedBox(height: 30),
+            LogoButton(
+              icon: AppIcons().googleIcon,
+              text: 'Google SignIn',
+              callback: () {
+                googleLogin(ref, context);
+              },
+            ),
+          ],
         ),
       ),
     );
